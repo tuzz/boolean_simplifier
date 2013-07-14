6e6be6de35ad2bc29045ed47cc639b4f82cf1bfc
Chris Patuzzo <chris@patuzzo.co.uk>
2013-07-14 22:31:03 +0100

    Implement the top-level parser

--- lib/boolean_simplifier/expression_parser.rb
+++ lib/boolean_simplifier/expression_parser.rb
@@ -1,7 +1,16 @@
 module ExpressionParser
   def parse(string)
+    parser = BooleanExpressionParser.new
 
+    ast = parser.parse(string)
+    if ast.nil?
+      raise ParseError, parser.failure_reason
+    end
+
+    ast.to_expression
   end
+
+  class ::ParseError < StandardError; end
 
   extend self
 end
--- spec/expression_parser_spec.rb
+++ spec/expression_parser_spec.rb
@@ -1,5 +1,27 @@
 require "spec_helper"
 
 describe ExpressionParser do
-  it "has some specs"
+
+  context "when given a valid string" do
+    let(:string) { "a && !(b || c)" }
+
+    it "returns an nested expression" do
+      subject.parse(string).should eq(
+        Conjunction.new(
+          "a", Negation.new(Disjunction.new("b", "c"))
+        )
+      )
+    end
+  end
+
+  context "when given an invalid string" do
+    let(:string) { "lol wut" }
+
+    it "raises a parse error" do
+      expect {
+        subject.parse(string)
+      }.to raise_error(ParseError)
+    end
+  end
+
 end
