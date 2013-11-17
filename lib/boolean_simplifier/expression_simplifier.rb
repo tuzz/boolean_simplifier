c800691c407d44ed6a6256c33426ff8ff08b2ca3
Chris Patuzzo <chris@patuzzo.co.uk>
2013-11-17 20:38:09 +0000

    Refactor and un-roll some of the recursion

--- lib/boolean_simplifier/expression_simplifier.rb
+++ lib/boolean_simplifier/expression_simplifier.rb
@@ -1,22 +1,38 @@
 module ExpressionSimplifier
   def simplify(expression)
-    return expression unless expression.respond_to?(:parts)
+    until_no_improvement(expression) do
+      simplify_parts(expression)
+      simplify_root(expression)
+    end
+  end
 
-    previous = expression.dup
+  private
+  def until_no_improvement(expression, &block)
+    begin
+      break if trivial?(expression)
+      previous = expression.dup
+      expression = yield
+    end until previous == expression
 
+    expression
+  end
+
+  def simplify_parts(expression)
     expression.parts.each.with_index do |part, index|
       expression.parts[index] = simplify(part)
     end
+  end
 
+  def simplify_root(expression)
     Rule.collection.each do |rule|
       expression = rule.simplify(expression)
     end
 
-    if previous == expression
-      expression
-    else
-      simplify(expression)
-    end
+    expression
+  end
+
+  def trivial?(expression)
+    !expression.respond_to?(:parts)
   end
 
   extend self
